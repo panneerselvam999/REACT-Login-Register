e561ad042455db0f708d1ad30d1a4fbc3ebe66b2
panneerselvam999 <npspanneerselvam999@gmail.com>
2024-04-27 14:53:41 -0500

    hide loding

--- Login-Register/src/components/RegisterPage.jsx
+++ Login-Register/src/components/RegisterPage.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "../css/RegisterPage.css";
 
 export const RegisterPage = () => {
-
     const [error, setError] = useState({
         email: { required: false },
         password: { required: false },
         name: { required: false },
-        custom_error: null
-    })
+        custom_error: null,
+    });
 
+    const [loading, setLoading] = useState(false);
 
     return (
         <>
@@ -22,49 +22,65 @@ export const RegisterPage = () => {
                                 <div className="form-group">
                                     <label
                                         htmlFor="exampleInputEmail1"
-                                        className="text-uppercase">
+                                        className="text-uppercase"
+                                    >
                                         Name
                                     </label>
 
                                     <input type="text" className="form-control" name="name" />
-                                    {error.name.required ? (<span className="text-danger">Name is required.</span>) : null}
+                                    {error.name.required ? (
+                                        <span className="text-danger">Name is required.</span>
+                                    ) : null}
                                 </div>
                                 <div className="form-group">
                                     <label
                                         htmlFor="exampleInputEmail1"
-                                        className="text-uppercase">
+                                        className="text-uppercase"
+                                    >
                                         Email
                                     </label>
 
                                     <input type="text" className="form-control" name="email" />
-                                    {error.email.required ? (<span className="text-danger">Email is required.</span>) : null}
+                                    {error.email.required ? (
+                                        <span className="text-danger">Email is required.</span>
+                                    ) : null}
                                 </div>
                                 <div className="form-group">
                                     <label
                                         htmlFor="exampleInputPassword1"
-                                        className="text-uppercase">
+                                        className="text-uppercase"
+                                    >
                                         Password
                                     </label>
                                     <input
                                         className="form-control"
                                         type="password"
-                                        name="password" />
-                                    {error.password.required ? (<span className="text-danger">Password is required.</span>) : null}
+                                        name="password"
+                                    />
+                                    {error.password.required ? (
+                                        <span className="text-danger">Password is required.</span>
+                                    ) : null}
                                 </div>
                                 <div className="form-group">
                                     <span className="text-danger">
-                                        {error.custom_error ? (<p>Custom Error Message!</p>) : null}
+                                        {error.custom_error ? <p>Custom Error Message!</p> : null}
                                     </span>
-                                    <div className="text-center">
-                                        <div className="spinner-border text-primary " role="status">
-                                            <span className="sr-only">Loading...</span>
+                                    {loading ? (
+                                        <div className="text-center">
+                                            <div
+                                                className="spinner-border text-primary "
+                                                role="status"
+                                            >
+                                                <span className="sr-only">Loading...</span>
+                                            </div>
                                         </div>
-                                    </div>
+                                    ) : null}
 
                                     <input
                                         type="submit"
                                         className="btn btn-login float-right"
-                                        value="Register" />
+                                        value="Register"
+                                    />
                                 </div>
                                 <div className="clearfix"></div>
                                 <div className="form-group">
